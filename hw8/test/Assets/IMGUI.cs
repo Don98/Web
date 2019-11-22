e9d4d0ad45e4ee5ddab869a8cc2e4fd53becd04f
Don <ZHONGDAchenqd6@163.com>
2019-11-22 12:52:06 +0800

    hw8

---
+++ hw8/test/Assets/IMGUI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Health))]
+public class IMGUI : MonoBehaviour
+{
+    private GameObject Character;
+	private GameObject people;
+    private float curHP;
+    private float fullHP;
+    private void Start() {
+        Character = this.gameObject;
+		Character.AddComponent<Health>();
+    }
+
+    private void OnGUI() {
+        if(GUI.Button(new Rect(UnityEngine.Screen.width/2 - 50,UnityEngine.Screen.height/2 - 40,80,40),"Hurt"))
+        {
+            Character.GetComponent<Health>().Hurt();
+        }
+        if(GUI.Button(new Rect(UnityEngine.Screen.width/2 + 50,UnityEngine.Screen.height/2 - 40,80,40),"Recover"))
+        {
+            Character.GetComponent<Health>().Recover();
+        }
+
+        curHP = Character.GetComponent<Health>().curHP;
+        fullHP = Character.GetComponent<Health>().fullHP;
+
+        GUI.HorizontalScrollbar(new Rect(UnityEngine.Screen.width/2 - 42,UnityEngine.Screen.height/2 +20,170,20), 0.0f, curHP, 0.0f, fullHP);
+    }
+}
+
