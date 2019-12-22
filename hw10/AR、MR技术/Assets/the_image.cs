b24803593c05386f5af7aea186d410ae414e4380
Don <ZHONGDAchenqd6@163.com>
2019-12-22 22:23:20 +0800

    update

---
+++ hw10/AR、MR技术/Assets/the_image.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+[System.Obsolete]
+public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vb;
+    public Animator ani;
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        ani.SetBool("IsRun", true);
+        Debug.Log("run");
+    }
+
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        ani.SetBool("IsRun", false);
+        Debug.Log("stop");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        if(vbb)
+        {
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
