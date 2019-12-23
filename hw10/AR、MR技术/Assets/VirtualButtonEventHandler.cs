6ae77f5b1f6d84d4bfeb0e08d2b42f3efa59df13
Don <ZHONGDAchenqd6@163.com>
2019-12-23 20:53:37 +0800

    updata

---
+++ hw10/AR、MR技术/Assets/VirtualButtonEventHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using Vuforia;
+[System.Obsolete]
+public class VirtualButtonEventHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+
+    public VirtualButtonBehaviour vb;
+    public Animator animator;
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        animator.SetBool("start", false);
+        animator.SetBool("jump", true);
+        Debug.Log("jump");
+    }
+
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+
+        animator.SetBool("jump", false);
+        animator.SetBool("start", true);
+        Debug.Log("stop");
+    }
+
+  
+    void Start()
+    {
+        VirtualButtonBehaviour vbb = vb.GetComponent<VirtualButtonBehaviour>();
+        if (vbb)
+        {
+            vbb.RegisterEventHandler(this);
+        }
+    }
+
+
+    void Update()
+    {
+
+    }
+
+}
